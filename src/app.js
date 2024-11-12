8eefa0f2d995797941e92d084ebf71e148e00e6f
Byantes <byantessavage@gmail.com>
2024-11-12 01:39:34 +0000

    Set up routing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-// src/App.js
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="bg-blue-500 text-white p-8">
-      <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
-      <p className="mt-4">This is styled with Tailwind CSS!</p>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <nav className="bg-blue-600 text-white p-4">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="/" className="hover:text-gray-200">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/about" className="hover:text-gray-200">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/projects" className="hover:text-gray-200">
+                Projects
+              </a>
+            </li>
+            <li>
+              <a href="/contact" className="hover:text-gray-200">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
