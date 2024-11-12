f781a4fc06da188fb5e7d70df259647833d9116c
Byantes <byantessavage@gmail.com>
2024-11-12 01:31:40 +0000

    Beginning

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// src/App.js
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-8">
+      <h1 className="text-3xl font-bold">Welcome to My Portfolio</h1>
+      <p className="mt-4">This is styled with Tailwind CSS!</p>
+    </div>
+  );
+}
+
+export default App;
